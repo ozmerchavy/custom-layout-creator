446ae37ec7e6da0752e629c8d03c5d8725705423
oz <ozmerchavy2@gmail.com>
2024-03-02 21:12:29 -0330

    drag drags

--- src/components/ElementPicker.jsx
+++ src/components/ElementPicker.jsx
@@ -5,54 +5,65 @@ import { store } from "../store";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 
+const icons = {
+  div: "container.png",
+  button: "button.png",
+};
 
-function initDrag(event, type){
-  const coords = [event.pageX, event.pageY]
-  store.dispatch(startDrag({ type, coords }))
+function initDrag(event, type) {
+  const coords = [event.pageX, event.pageY];
+  store.dispatch(startDrag({ type, coords }));
 
   const onMouseMove = (event) => {
     const coords = [event.pageX, event.pageY];
     store.dispatch(moveDrag(coords));
   };
 
-  const quitDrags = ()=>{
+  const quitDrags = () => {
     store.dispatch(endDrag());
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', quitDrags);
-  }
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", quitDrags);
+  };
 
-  document.addEventListener('mousemove', onMouseMove)
-  document.addEventListener('mouseup', quitDrags)
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", quitDrags);
 }
 
-
-
+function ItemOption({ type }) {
+  return (
+    <span draggable="false" className={styles.ElementButton}>
+      <img draggable="false" src={icons[type]} alt="" />
+    </span>
+  );
+}
 
 export function DragThingy() {
   const drag = useSelector((store) => store.canvasElements.drag);
 
-  if (!drag) return;
+  if (!drag) return;  
 
   const [x, y] = drag.coords;
 
-  return <div style={{
-    width: '50px',
-    height: '50px',
-    borderRadius: '50px',
-    backgroundColor: 'magenta',
-    position: 'absolute',
-    left: `${x - 25}px`,
-    top: `${y - 25}px`,
-  }}>{drag.type}</div>
+  return (
+    <div
+      draggable="false"
+      style={{
+        position: "absolute",
+        left: `${x - 25}px`,
+        top: `${y - 25}px`,
+      }}
+    >
+      <ItemOption type={drag.type} />
+    </div>
+  );
 }
 
-function ElementButton({ text, icon, type }) {
-
+function ElementButton({ type }) {
   return (
-    <span className={styles.ElementButton} onMouseDown={(event)=>initDrag(event, type)}>
-      <img src={icon} alt="" />
-      <span>{text}</span>
-    </span>
+    <div onMouseDown={(event) => initDrag(event, type)}>
+      <ItemOption type={type} />
+      <span draggable="false">{type}</span>
+    </div>
   );
 }
 
@@ -61,7 +72,6 @@ export default function ElementPicker() {
     <article className={`${layout.ElementPicker} ${styles.ElementPicker}`}>
       <ElementButton text="Button" icon="button.png" type="button" />
       <ElementButton text="Container" icon="container.png" type="div" />
-      
     </article>
   );
 }
